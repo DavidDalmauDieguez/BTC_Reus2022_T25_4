85f0478b2e6bd23e32752b76bddc017ff6cd466b
OctavioBernalGH <103035621+OctavioBernalGH@users.noreply.github.com>
2022-05-21 19:02:36 +0200

    Se eliminan imports innecesarios.

--- UD25_ejercicio4/src/main/java/com/example/demo/service/PeliculaServiceImpl.java
+++ UD25_ejercicio4/src/main/java/com/example/demo/service/PeliculaServiceImpl.java
@@ -4,32 +4,23 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.dao.PeliculasDAO;
 import com.example.demo.dto.Peliculas;
 
 @Service
-public class PeliculaServiceImpl implements PeliculaService{
+public class PeliculaServiceImpl implements PeliculaService {
 
 	// Utilizar metodos DAO heredados de JPA
 	@Autowired
 	PeliculasDAO peliculasDao;
 
 	@Override
-	public List<Peliculas> listarPeliculas() {		
+	public List<Peliculas> listarPeliculas() {
 		return peliculasDao.findAll();
 	}
 
 	@Override
-	public Peliculas crearPeliculas(Peliculas pelicula) {		
+	public Peliculas crearPeliculas(Peliculas pelicula) {
 		return peliculasDao.save(pelicula);
 	}
 
@@ -47,5 +38,5 @@ public class PeliculaServiceImpl implements PeliculaService{
 	@Override
 	public Peliculas buscarPeliculas(Long codigo) {
 		return peliculasDao.findById(codigo).get();
-	}	
+	}
 }
--- UD25_ejercicio4/src/main/java/com/example/demo/service/SalasServiceImpl.java
+++ UD25_ejercicio4/src/main/java/com/example/demo/service/SalasServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.dao.SalasDAO;
 import com.example.demo.dto.Salas;
 
